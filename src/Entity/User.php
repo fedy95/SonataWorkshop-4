ff10f9e34932c614071743831049f740697484fa
fedorov <fedorov.ilya.s@mail.ru>
2019-01-23 18:51:34 +0300

    update user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -18,9 +19,182 @@ class User extends BaseUser
 	 */
 	protected $id;
 
+	/**
+	 * @var string
+	 * @Assert\Length(
+	 *      max = 255,
+	 * )
+	 * @ORM\Column(type="string", length=255, nullable=false)
+	 */
+	protected $firstName;
+
+	/**
+	 * @var string
+	 * @Assert\Length(
+	 *      max = 255,
+	 * )
+	 * @ORM\Column(type="string", length=255, nullable=false)
+	 */
+	protected $lastName;
+
+	/**
+	 * @var string
+	 * @Assert\Length(
+	 *      max = 255,
+	 * )
+	 * @ORM\Column(type="string", length=255, nullable=true)
+	 */
+	protected $middleName;
+
+	/**
+	 * @ORM\Column(type="date", length=255, nullable=true)
+	 */
+	protected $dateOfBirth;
+
+	/**
+	 * @var string
+	 * @Assert\Length(
+	 *      max = 255,
+	 * )
+	 * @ORM\Column(type="string", length=255, nullable=true)
+	 */
+	protected $sex;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(type="string", length=255, nullable=true)
+	 */
+	protected $phone;
+
 	public function __construct()
 	{
 		parent::__construct();
-		// your own logic
+	}
+
+	/**
+	 * Set firstName
+	 *
+	 * @param string $firstName
+	 *
+	 * @return User
+	 */
+	public function setFirstName($firstName) : User
+	{
+		$this->firstName = $firstName;
+
+		return $this;
+	}
+
+	/**
+	 * Get firstName
+	 * @return string
+	 */
+	public function getFirstName() : ?string
+	{
+		return $this->firstName;
+	}
+
+	/**
+	 * Set middleName
+	 *
+	 * @param string $middleName
+	 *
+	 * @return User
+	 */
+	public function setMiddleName($middleName) : User
+	{
+		$this->middleName = $middleName;
+
+		return $this;
+	}
+
+	/**
+	 * Get middleName
+	 * @return string
+	 */
+	public function getMiddleName() : ?string
+	{
+		return $this->middleName;
+	}
+
+	/**
+	 * Set lastName
+	 *
+	 * @param string $lastName
+	 *
+	 * @return User
+	 */
+	public function setLastName($lastName) : User
+	{
+		$this->lastName = $lastName;
+
+		return $this;
+	}
+
+	/**
+	 * Get lastName
+	 * @return string
+	 */
+	public function getLastName() : ?string
+	{
+		return $this->lastName;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getDateOfBirth()
+	{
+		return $this->dateOfBirth;
+	}
+
+	/**
+	 * @param mixed $dateOfBirth
+	 *
+	 * @return User
+	 */
+	public function setDateOfBirth($dateOfBirth)
+	{
+		$this->dateOfBirth = $dateOfBirth;
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getSex() : ?string
+	{
+		return $this->sex;
+	}
+
+	/**
+	 * @param string $sex
+	 *
+	 * @return User
+	 */
+	public function setSex(string $sex) : User
+	{
+		$this->sex = $sex;
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPhone() : ?string
+	{
+		return $this->phone;
+	}
+
+	/**
+	 * @param string $phone
+	 *
+	 * @return User
+	 */
+	public function setPhone(string $phone) : User
+	{
+		$this->phone = $phone;
+		return $this;
 	}
 }
